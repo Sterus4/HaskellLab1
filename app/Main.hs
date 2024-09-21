71cd6aead866c3f8fb8d7759eebbf8e3994f206d
Sterus4 <368581@niuitmo.ru>
2024-09-21 11:33:53 +0300

    new Solutions

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,15 @@
 module Main (main) where
-import Euler15 (solution)
+import Euler15 (solution, lazySolution)
 import Euler16 (sumOfNumeral1, sumOfNumeral2, sumOfNumeralTailRec, sumOfNumeralMap)
 
 
 main :: IO ()
 main = do
     putStrLn "Euler 15:"
+    print $ lazySolution 20
     print $ solution 20
     putStrLn "Euler 16:"
     print $ sumOfNumeral1 (2 ^ (1000 :: Integer))
     print $ sumOfNumeral2 (2 ^ (1000 :: Integer))
     print $ sumOfNumeralTailRec (2 ^ (1000 :: Integer)) 0
     print $ sumOfNumeralMap (2 ^ (1000 :: Integer))
-    putStrLn $ "Hello World"
--- src/Euler15.hs
+++ src/Euler15.hs
@@ -1,4 +1,4 @@
-module Euler15 (recursiveSolution, solution) where
+module Euler15 (recursiveSolution, lazySolution, solution) where
 
 
 recSol :: Integer -> Integer -> Integer
@@ -9,8 +9,29 @@ recSol m n = recSol m (n - 1) + recSol (m - 1) n
 recursiveSolution :: Integer -> Integer
 recursiveSolution n = recSol n n
 
-solution :: Int -> Integer
-solution n = 
-    pascall !! n !! n where 
-        pascall = temp : map (scanl1 (+)) pascall where
+getMiddle :: [a] -> a 
+getMiddle l1 = 
+    let cent = div (length l1) 2 in
+        l1 !! cent
+
+addLists :: (Num a) => [a] -> [a] -> [a]
+addLists = zipWith (+)
+
+pascall :: (Eq a1, Num a1, Num a2) => a1 -> [a2]
+pascall 1 = [1]
+pascall n = 
+    let prev = pascall (n - 1)
+        zeroPrev = 0 : prev
+        prevZero = prev ++ [0]
+    in 
+    addLists zeroPrev prevZero
+solution :: Integer -> Integer
+solution n = getMiddle (pascall (n * 2 + 1))
+
+
+
+lazySolution :: Int -> Integer
+lazySolution n = 
+    pascall1 !! n !! n where 
+        pascall1 = temp : map (scanl1 (+)) pascall1 where
             temp = repeat 1
